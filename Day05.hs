7683a351ab727907265980da8ff2c44c8838eb65
Jesse Holzman <jesseholzman96@gmail.com>
2020-12-05 18:06:56 -0500

    Improve day 5 solutions.

--- Day05.hs
+++ Day05.hs
@@ -10,14 +10,11 @@ import Util
 
 
 seatID :: String -> Int
-seatID s = toNum row * 8 + toNum col
- where
-  toNum      = foldl' (\acc x -> acc * 2 + fromEnum (x `elem` "BR")) 0
-  (row, col) = splitAt 7 s
+seatID = foldl' (\acc x -> acc * 2 + fromEnum (x `elem` "BR")) 0
 
 
 findSeat :: [Int] -> Int
-findSeat ss = (snd . head) $ filter (liftA2 (/=) fst snd) (zip ss [head ss ..])
+findSeat = head . liftA2 (\\) (\l -> [minimum l .. maximum l]) id
 
 
 solveDay05 :: IO ()
@@ -25,5 +22,5 @@ solveDay05 = runSolver Solver
   { inputFile  = "inputs/05.txt"
   , parseInput = map seatID . lines
   , part1      = Just . maximum
-  , part2      = Just . findSeat . sort
+  , part2      = Just . findSeat
   }
