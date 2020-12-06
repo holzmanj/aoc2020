981af7f3d0a2cd4b461d3021cb0f8641bc14b18f
Jesse Holzman <jesseholzman96@gmail.com>
2020-12-06 09:46:09 -0500

    Add solutions for day 6.

---
+++ Day06.hs
@@ -0,0 +1,15 @@
+
+import Data.List
+import Data.List.Split
+
+import Util
+
+
+solveDay06 :: IO ()
+solveDay06 = runSolver Solver
+  { inputFile  = "inputs/06.txt"
+  , parseInput = splitOn "\n\n"
+  , part1      = Just . sum . map (length . nub . filter (/= '\n'))
+  , part2      = Just . sum . map (length . foldr1 intersect . lines)
+  }
+
