a390653a8c9a260477cc18648d408b9f77b725d8
Jesse Holzman <jesseholzman96@gmail.com>
2020-12-07 20:04:03 -0500

    Pull out benchmarking into separate function

--- Util.hs
+++ Util.hs
@@ -14,12 +14,16 @@ data Solver a b c = Solver
 runSolver :: (Show b, Show c) => Solver a b c -> IO ()
 runSolver solver = do
   input <- parseInput solver <$> readFile (inputFile solver)
-  putStrLn "-- SOLUTIONS --"
   putStrLn $ "Part 1 (silver):  " ++ maybeShow (part1 solver input)
   putStrLn $ "Part 2 (gold):    " ++ maybeShow (part2 solver input)
-  putStrLn "\n\n-- PERFORMANCE --"
+  where maybeShow x = maybe "Faild to find a solution" show x
+
+
+benchmark :: Solver a b c -> IO ()
+benchmark solver = do
+  input <- parseInput solver <$> readFile (inputFile solver)
   defaultMain
     [ bench "part 1 (silver)" $ whnf (part1 solver) input
     , bench "part 2 (gold)" $ whnf (part2 solver) input
     ]
-  where maybeShow x = maybe "Faild to find a solution" show x
+
